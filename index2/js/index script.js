f4d259c5dc42e7593695ce16a4fbc3101362a771
AYOLMO <103184807+AYOLMO@users.noreply.github.com>
2024-03-03 11:10:39 +0800

    Add files via upload

--- index2/js/index script.js
+++ index2/js/index script.js
@@ -9,7 +9,7 @@ for (let i = 0; i < navbarItem.length; i++) {
 }
 
 var typed = new Typed(".multiple-text", {
-    strings: ["JBL 4329P 仅需13500元！！！","生活充满乐趣", "文件共享更加便捷", "网页游戏集中起来"],
+    strings: ["JBL 4329P 仅需12700元！！！","生活充满乐趣", "文件共享更加便捷", "网页游戏集中起来"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
